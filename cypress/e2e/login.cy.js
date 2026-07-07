2c1e0b6044b7fa4d30073c2559a9daa0d8620366
Leandrordsc <leandrordsc@gmail.com>
2026-07-06 23:52:31 -0300

    Ajusta scripts e teste de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,25 @@
 describe('Login', () => {
-  beforeEach(() =>{
+  beforeEach(() => {
     cy.visit('/')
   })
-  it('Login com dados validos deve permitir entrada no sistema', () => {   
-    cy.fixture('credenciais').then(credenciais =>{
+
+  it('Login com dados validos deve permitir entrada no sistema', () => {
+    cy.fixture('credenciais').then((credenciais) => {
       cy.get('#username').click().type(credenciais.valida.usuario)
       cy.get('#senha').click().type(credenciais.valida.senha)
-      })
-    cy.contains('button','Entrar').click()
-    cy.contains('h4','Realizar Transferência').should('be.visible')
+    })
+
+    cy.contains('button', 'Entrar').click()
+    cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
-   it('Login com dados invalidos deve apresentar mensagem de erro', () => {
-    cy.fixture('credenciais').then(credenciais =>{
+  it('Login com dados invalidos deve apresentar mensagem de erro', () => {
+    cy.fixture('credenciais').then((credenciais) => {
       cy.get('#username').click().type(credenciais.invalida.usuario)
       cy.get('#senha').click().type(credenciais.invalida.senha)
     })
-    cy.contains('button','Entrar').click()
+
+    cy.contains('button', 'Entrar').click()
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
-
 })
