300bccaf3319deefe04faa9d6ff5ed1602f44ec8
Leandrordsc <leandrordsc@gmail.com>
2026-07-07 22:06:48 -0300

    transferencias

--- cypress/e2e/transf.cy.js
+++ cypress/e2e/transf.cy.js
@@ -1,1 +1,13 @@
-describe
+describe("Transferencias", () => {
+  beforeEach(() => {
+    cy.visit("/")
+    cy.fixture("credenciais").then((credenciais) => {
+      cy.get("#username").click().type(credenciais.valida.usuario);
+      cy.get("#senha").click().type(credenciais.valida.senha);
+    })
+    cy.contains("button", "Entrar").click();
+ })
+
+it("Deve transferir quando informo dados e valor validos", () => {});
+
+}) 
