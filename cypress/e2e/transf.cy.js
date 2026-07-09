d856fb6a7ce8c0c87b18e313edd837a67e9b98e5
Leandrordsc <leandrordsc@gmail.com>
2026-07-08 23:12:56 -0300

    Adicionando o relatorio HTMl do mochawesome

--- cypress/e2e/transf.cy.js
+++ cypress/e2e/transf.cy.js
@@ -6,11 +6,14 @@ describe("Transferencias", () => {
 
   it("Deve transferir quando informo dados e valor validos", () => {
     cy.realizarTransferencia('Maria','João', '150')
-
     cy.verificarMensagemNoToast("Transferência realizada!")
   })
   it("Deve apresentar erro quanto tentar transferir valor maior que 5 mil sem o token", () => {
     cy.realizarTransferencia('João','Maria', '5000.01')
     cy.verificarMensagemNoToast("Autenticação necessária para transferências acima de R$5.000,00.")
+  })
+   it("Deve apresentar erro quanto tentar transferir valor menor que R$10,00", () => {
+    cy.realizarTransferencia('João','Maria', '9.99')
+    cy.verificarMensagemNoToast("O valor da transferência deve ser maior ou igual a R$10,00.")
   })
 })
